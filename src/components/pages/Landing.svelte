4b668d8f7ef769ddcbec188b25a255cd396fa820
NoNameWrath <mabejo2005@gmail.com>
2022-10-10 23:00:56 +0530

    .

---
+++ src/components/pages/Landing.svelte
@@ -0,0 +1,40 @@
+<body class="typewriter p-8 text-3xl">
+	<h1>Abejo Michael (Wrath-dev)</h1>
+</body>
+
+<style>
+	body {
+		display: flex;
+		justify-content: center;
+	}
+
+	.typewriter h1 {
+		color: #fff;
+		font-family: monospace;
+		overflow: hidden;
+		border-right: 0.15em solid black;
+		white-space: nowrap;
+		margin: 0 auto;
+		letter-spacing: 0.15em;
+		animation: typing 4s steps(30, end), blink-caret 0.5s step-end infinite;
+	}
+
+	@keyframes typing {
+		from {
+			width: 0;
+		}
+		to {
+			width: 100%;
+		}
+	}
+
+	@keyframes blink-caret {
+		from,
+		to {
+			border-color: transparent;
+		}
+		50% {
+			border-color: black;
+		}
+	}
+</style>
